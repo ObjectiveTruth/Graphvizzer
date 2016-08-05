198ed822f24e6bc2e5bcb6ecbf9d5e6f6b4ed7ba
ObjectiveTruth <miguel@objectivetruth.ca>
2016-08-04 22:43:58 -0400

    Added registration flow without backend

--- app/controllers/RegisterController.scala
+++ app/controllers/RegisterController.scala
@@ -3,9 +3,10 @@ package controllers
 import javax.inject.Inject
 
 import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.ws.{WSClient, WSResponse}
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 import slick.driver.JdbcProfile
 import common.constants._
 import models.SlackCodeForTokenResponse
@@ -19,49 +20,53 @@ import scala.util.{Failure, Success, Try}
 class RegisterController @Inject()(dbConfigProvider: DatabaseConfigProvider)(ws: WSClient)(actorSystem: ActorSystem)
                                   (implicit exec: ExecutionContext)
   extends Controller {
+    val config = ConfigFactory.load("strings.conf")
+    val UNEXPECTED_SYSTEM_ERROR = config.getString("SystemMessages.UnexpectedSystemError")
+    val REGISTER_SUCCESS = config.getString("BusinessMessages.SuccessfulRegister")
+
     val dbConfig = dbConfigProvider.get[JdbcProfile]
 
-    def registerTeam(code: Int) = Action.async { implicit request =>
-      //https://slack.com/api/oauth.access?
-        // client_id=29667068068.63519026177
-        // &client_secret=25172e4a0342ca40fffeed99ad8ea416
-        // &code=32435995891.66111737364.f4a625b2e7
-      exchangeCodeForTokenAndWriteToDatabase(code)
-        Future{Ok(s"hello, ${code}")}
+    def registerTeam(code: String) = Action.async { implicit request =>
+        exchangeCodeForToken(code)
     }
 
     def teamStatus = Action.async { implicit request =>
-        Future{Ok("fjeklsfje")}
+        Future{Ok("To Implement")}
     }
 
-    def exchangeCodeForTokenAndWriteToDatabase(code: Int): Unit = {
+    def exchangeCodeForToken(code: String): Future[Result] = {
         ws.url(SLACK.OAUTH_ENDPOINT)
-          .withQueryString(
+            .withQueryString(
               "client_id" -> SLACK.CLIENT_ID,
               "client_secret" -> SLACK.APP_SECRET,
-              "code" -> code.toString
-          )
-          .get()
-          .onComplete({
-              case Success(s: WSResponse) => {
-                  Logger.info(s"Received response from Slack oauth endpoint with status: ${s.status}, body: ${s.body}")
-                  if(s.status >= 200 && s.status < 300 ) {
-                      s.json.validate[SlackCodeForTokenResponse] match {
-                          case slackCodeForTokenResponse: JsSuccess[SlackCodeForTokenResponse] => {
+              "code" -> code
+            )
+            .get()
+            .flatMap{ s =>
+              Logger.info(s"Received response from Slack oauth endpoint with status: ${s.status}, body: ${s.body}")
+              if(s.status >= 200 && s.status < 300 ) {
+                  Future{Ok("")}
+                  s.json.validate[SlackCodeForTokenResponse] match {
+                      case slackCodeForTokenResponse: JsSuccess[SlackCodeForTokenResponse] => {
 
-                              if(slackCodeForTokenResponse.value.ok) {
-                                  Logger.info("Token Received Successfully!")
-                              }else {
-                                  Logger.error(s"Slack rejected the code when trying to exchange for token: " +
-                                    slackCodeForTokenResponse)
-                              }
-
+                          if(slackCodeForTokenResponse.value.ok) {
+                              Logger.info("Token Received Successfully!")
+                              Future{Ok(REGISTER_SUCCESS)}
+                          }else {
+                              Logger.error(s"Slack rejected the code when trying to exchange for token: " +
+                                slackCodeForTokenResponse.value)
+                              Future{Ok(UNEXPECTED_SYSTEM_ERROR)}
                           }
-                          case _ => { Logger.error("Invalid JSON when exchanging code for token with Slack") }
+
                       }
-                  }else { Logger.error("Bad response Slack when exchanging code for access token") }
+                      case _ =>
+                          Logger.error("Invalid JSON when exchanging code for token with Slack")
+                          Future{Ok(UNEXPECTED_SYSTEM_ERROR)}
+                  }
+              }else {
+                  Logger.error("Unexpected response Slack when exchanging code for access token")
+                  Future{Ok(UNEXPECTED_SYSTEM_ERROR)}
               }
-              case Failure(f: Throwable) => { Logger.error("Could not exchange Slack code for access token", f) }
-          })
+        }
     }
 }
